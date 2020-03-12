a5eacb23823b223fb71dd3e422f2f93f9727a3fc
Xintao <hunterlxt@live.com>
2020-03-13 00:10:50 +0800

    Add macro test
    Signed-off-by: Xintao <hunterlxt@live.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 mod result_option;
 mod transmute;
 
-fn main() {}
+use std::thread;
+
+use clap::{App, Arg, SubCommand};
+
+fn main() {
+    macro_rules! veec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+    let v = veec![2, 3, 4];
+
+    println!("{:?}", v);
+}
