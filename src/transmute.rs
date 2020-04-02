df895aea32d68d68044079d8a37e0602ba18bb2c
TXXT <hunterlxt@live.com>
2020-04-02 13:53:48 +0800

    update
    Signed-off-by: TXXT <hunterlxt@live.com>

--- src/transmute.rs
+++ src/transmute.rs
@@ -20,6 +20,16 @@ pub fn test_str_to_u8() {
     println!("{:?}", v);
 }
 
+pub fn test_chain_result() {
+    let mut b1 = Box::new(10 as i32);
+    *b1 += 10;
+    let mut b2 = b1.clone();
+    *b2 += 100;
+    let ptr: *mut std::ffi::c_void = unsafe { std::mem::transmute(b1) };
+    let content: &i32 = unsafe { std::mem::transmute(ptr) };
+    assert_eq!(*content, 10);
+}
+
 fn foo(mut i: i32) -> i32 {
     i *= 2;
     i
