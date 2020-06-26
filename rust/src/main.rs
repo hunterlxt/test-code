2226a4ae56f4c84688ede4abfd796fce9ac8c7ea
Xintao <hunterlxt@live.com>
2020-06-26 18:30:46 +0800

    checkpoint
    Signed-off-by: Xintao <hunterlxt@live.com>

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,42 @@
-use test_rust::timer_future;
+use futures::channel::mpsc;
+use futures::executor::block_on;
+use futures::future::{Either, Future, FutureExt, Shared, TryFuture, TryFutureExt};
+use futures::sink::{Sink, SinkExt};
+use futures::stream::{Stream, StreamExt};
+use futures::task::{Context, Poll};
+use tokio::runtime;
+use tokio::time;
+
+use std::collections::VecDeque;
+use std::pin::Pin;
+use std::thread;
+use std::time as stdtime;
 
 fn main() {
-    timer_future::run();
+    request(|s| Box::new(s));
+}
+
+fn get_guard(m: &std::sync::Mutex<i32>) -> std::sync::MutexGuard<i32> {
+    let data = m.lock().unwrap();
+    data
+}
+
+fn get_future(flag: bool) -> Box<dyn Future<Output = ()>> {
+    if flag {
+        Box::new(async move {
+            println!("true");
+        })
+    } else {
+        Box::new(async move {
+            println!("false");
+        })
+    }
+}
+
+fn request<F>(mut exec: F)
+where
+    F: FnMut(String) -> Box<String> + 'static,
+{
+    let data = exec("abc".to_owned());
+    println!("{}", *data);
 }
